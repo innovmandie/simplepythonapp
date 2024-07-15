5740b0c5f749f4ab16d428edd61efcad11144abe
rachidsgar76 <rachid.rahal@normandie.gouv.fr>
2024-07-15 12:55:45 +0000

    new version app

--- app.py
+++ app.py
@@ -1,12 +1,20 @@
-from flask import Flask, render_template, url_for, redirect
+from flask import Flask, render_template
+import tablib
+import os
+from flask_bootstrap import Bootstrap4
+from flask import Flask
 
+import pandas as pd
 
-app = Flask(__name__, static_folder="/static/")
-
-
-@app.route('/')
+app = Flask (__name__)
+dataset = pd.read_csv('titanic3.csv')
+ 
+@app.route("/")
 def index():
-    return render_template('index.html')
-
+    data = dataset.head(n=10)
+    data = data.to_html()
+    #return dataset.html
+    return render_template('index.html', data=data)
+ 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
